a50dda9de7ba93c6f9a49604c269e9175b208a4d
100tx <100tx@LAPTOP-L8SU3CG1>
2022-08-08 20:43:21 +0530

    first commit

---
+++ Day1 Git and GitHub/src/com/capgemini/git/EclipseWithGithub.java
@@ -0,0 +1,10 @@
+package com.capgemini.git;
+
+public class EclipseWithGithub {
+
+	public static void main(String[] args)
+	{
+      System.out.println("Welcome To Capgemini C2tc Program");
+	}
+
+}

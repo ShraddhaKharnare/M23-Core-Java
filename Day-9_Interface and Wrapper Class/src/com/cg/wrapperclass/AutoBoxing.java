9b61f17678e1c088cc1fa43f87696b02bf52542d
100tx <100tx@LAPTOP-L8SU3CG1>
2022-08-16 14:33:12 +0530

    nine commit

---
+++ Day-9_Interface and Wrapper Class/src/com/cg/wrapperclass/AutoBoxing.java
@@ -0,0 +1,12 @@
+package com.cg.wrapperclass;
+
+public class AutoBoxing 
+{
+	public static void main(String[] args) 
+	{
+		float a=56.4f;
+	    Float b=a; //conver ting primitive to wrapper class object
+        Float f=new Float (87.9);
+        System.out.println(a+" "+b+" "+f);
+	}
+}
---
+++ Day-9_Interface and Wrapper Class/src/com/cg/wrapperclass/UnBoxing.java
@@ -0,0 +1,15 @@
+package com.cg.wrapperclass;
+
+public class UnBoxing {
+
+	public static void main(String[] args)
+	{
+		Integer a=23;
+		int b=a;               //conversion of Wrapper obj to primitive
+		System.out.println(a);
+		System.out.println(b);
+		
+
+	}
+
+}
